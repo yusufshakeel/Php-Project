8a650c29977f91c3f316bf9b694fb00072964776
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2014-02-12 12:05:12 +0530

    Initial - hello world index.php

---
+++ src/hello-world/index.php
@@ -0,0 +1,13 @@
+<!-- This will display 'Hello World' -->
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Hello World</title>
+    </head>
+    <body>
+        <?php
+            echo 'Hello World';
+        ?>
+    </body>
+</html>
