804f87739ded4c5a7bbeb3d2bd3d2ac8fbec6257
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2014-02-14 16:22:14 +0530

    Initial registration-form.php

---
+++ src/information-passing/registration-form.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Registration Form</title>
+	</head>
+	<body>
+		<form action="register2.php" method="post">
+			<p>Registration Form</p>
+			<label for="username">Username:</label>
+			<input type="text" name="username">
+			<label for="email">Email:</label>
+			<input type="text" name="email">
+			<input type="submit" value="Register!">
+		</form>
+	</body>
+</html>
